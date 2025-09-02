78823d93957976ae6ad68ee75b5d7ff75ae08394
franlecam <franleiva95@gmail.com>
2025-09-02 09:36:02 -0300

    Actualización

---
+++ Scripts de clases/Prácticas/Práctica del 09-01.R
@@ -0,0 +1,7 @@
+# 1 Construir cuatro vectores de datos, simluando una base de datos (algunas variables de tipo character y otras de tipo numeric)
+# 2 Generar un data frame  y cambiar el nombre en al menos una de las variables
+# 3 Construir un "sub data frame" entendido como un espacio más chico del data frame generado (puede ser recortando filas y/o columnas particulares)
+# 4 Calcular la media y la mediana de una o algunas variable/s numéricas. ¿Qué puede decir de los resultados?
+# 5 Calcular el desvío estándar, ¿qué tan volátiles son sus variables numéricas?
+# 6 Generar un codigo que devuelva la cantidad de resultados posibles por variable no numérica (puede ayudarse con la función table() u otras)
+# 7 Elaborar un data frame de iguales dimensiones al que se obtuvo en la CONSIGNA 3, crear una lista que los incluya a ambos y guardar la lista en formato RDS.
