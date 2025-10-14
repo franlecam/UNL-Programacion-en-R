3afd5cf3e1504f428e29ec7be45a96c706363ee8
franlecam <69166136+franlecam@users.noreply.github.com>
2025-10-14 14:42:42 -0300

    Sexta práctica

---
+++ Scripts de clases/Prácticas/Práctica del 09-29 (5ta).R
@@ -0,0 +1,18 @@
+#----------------------------------------------------------------
+### Práctica de Visualización de Datos con Plotly | Semana 29/9
+#----------------------------------------------------------------
+
+## Fecha límite de entrega: Domingo 12 de octubre 23:59hs
+
+
+## Para esta práctica, continúen trabajando con la base de datos que eligieron y exploraron en las prácticas anteriores. 
+
+# Consigna 1. Rehacer las 6 gráficas del práctico anterior de modo que sean interactivas. Pd: pueden usar plot_ly() o transformar gráficos de ggplot2 con ggplotly().
+
+
+## Consignas adicionales: A investigar! 
+
+# Consigna 2. Hacer un gráfico de líneas donde el eje X tenga un slider de tiempo y se pueda hacer zoom interactivo. Como pista, exploren cómo implementar esta funcionalidad usando el argumento rangeslider = list(visible = TRUE) dentro de layout().
+
+# Consigna 3. Crear un gráfico de dispersión donde se pueda elegir, mediante botones, qué variable va en el eje X y cuál en el eje Y. Pd: Explorar el uso del parámetro updatemenus.
+
