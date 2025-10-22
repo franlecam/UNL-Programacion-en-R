b3c15f132598cc409c3565606f0e4fbe099fc079
franlecam <69166136+franlecam@users.noreply.github.com>
2025-10-22 15:22:21 -0300

    Práctica 20-10

---
+++ Scripts de clases/Prácticas/Prácticas/Práctica del 20-10 (7ma).R
@@ -0,0 +1,20 @@
+#----------------------------------------------------------------
+### Práctica de mapas y dashboard  | Semana 20-10
+#----------------------------------------------------------------
+
+## Fecha límite de entrega: Domingo 26 de octubre 23:59hs
+
+
+## Para esta práctica, continúen trabajando con la base de datos que eligieron y exploraron en las prácticas anteriores. 
+
+# Consigna 1. Elaborar dos mapas utilizando datos de manera libre:
+
+## el primero debe ser un mapa de puntos
+
+## el segundo debe ser un mapa de polígonos (o áreas)
+
+## Consigna 2: elborar un dashboard (cantidad de solapas que deseen) cuyo contenido sean consginas de clases previas (tablas, gráficos estáticos, gráficos dinámicos o mapas).
+
+## No hay un contenido mínimo, pero se sugiere que intenten explorar el dashboard todo lo que necesiten.
+
+
